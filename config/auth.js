d9dba6eb2bebc21ecec0fe280087961269884c37
rakeshgupta <rakeshpriy143@gmail.com>
2016-03-19 04:28:39 +0530

    Login application

---
+++ config/auth.js
@@ -0,0 +1,21 @@
+module.exports = {
+
+    'facebookAuth' : {
+        'clientID'      : '1707490516165575', // your App ID
+        'clientSecret'  : 'aa4885b462c1ac372e7b4cd235ce70b4', // your App Secret
+        'callbackURL'   : 'http://localhost:8080/auth/facebook/callback'
+    },
+
+    'twitterAuth' : {
+        'consumerKey'       : 'SaWiag7yt37RPDvATzgD9w',
+        'consumerSecret'    : 'HWWUVS29oiQ2oC5IcfHG1Jm4CdIjp2Gc4I8VCLg3VA',
+        'callbackURL'       : 'http://localhost:8080/auth/twitter/callback'
+    },
+
+    'googleAuth' : {
+        'clientID'      : '757506392386-j7lghoo24tou6c1q3slcmae50unq3but.apps.googleusercontent.com',
+        'clientSecret'  : 'ZOmIdQH9IdBqakIXFYXV0QBG',
+        'callbackURL'   : 'http://localhost:8080/auth/google/callback'
+    }
+
+};
